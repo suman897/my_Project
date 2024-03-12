e296bdc45c0e302ddf17cab9d31c8923ac5f58b6
suman897 <vamanparui2000@gmail.com>
2024-03-12 23:09:38 +0530

    feat/zig-zag pattern

--- test.py
+++ test.py
@@ -1,16 +1,14 @@
 #program to print Zig Zag lines pattern
 
-def zigZag(length, height):
-    for i in range(length):
-        for j in range(height):
-            yield('*')
-
-
-def triangle(height):
-    for i in range(height+1):
-        print("*"*i)
-    for i in range(height+1):
-        print("*"*(height-i-1))
-
-print(triangle(5))
+def zigZagPattern(length = 5, height = 4):
+    length -= 1
+    for j in range(height):
+        for i in range(length):
+            print("  "*(i), end="*")
+            print()
+        for i in range(length):
+            print("  "*(length-i), end="*")
+            print()
+    print("*")
+print(zigZagPattern(5,4))
 
