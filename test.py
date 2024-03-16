a37a0c326f74c83f91a89d130771bb6422ca61d2
suman897 <vamanparui2000@gmail.com>
2024-03-16 15:42:10 +0530

    added function in the Pattern class

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 #program to print Zig Zag lines pattern
-
-def zigZagPattern(length = 5, height = 4):
-    length -= 1
-    for j in range(height):
-        for i in range(length):
-            print("  "*(i), end="*")
-            print()
-        for i in range(length):
-            print("  "*(length-i), end="*")
-            print()
-    print("*")
+class Pattern:
+    def zigZagPattern(length = 5, height = 4):
+        length -= 1
+        for j in range(height):
+            for i in range(length):
+                print("  "*(i), end="*")
+                print()
+            for i in range(length):
+                print("  "*(length-i), end="*")
+                print()
+        print("*")
 
 if __name__ == '__main__':
-    print(zigZagPattern(5,4))
+    print(Pattern.zigZagPattern(5,4))
 
